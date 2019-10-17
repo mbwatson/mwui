598eaad44fc7ef9f13279bb1ba2fb913474e208d
Matt Watson <mattwat@gmail.com>
2019-10-17 19:47:51 -0400

    add icons and lines

--- src/components/divider/divider.js
+++ src/components/divider/divider.js
@@ -3,6 +3,6 @@ import styled from 'styled-components'
 export const Divider = styled.div`
     width: 100%;
     height: 1px;
-    background-color: var(--color-grey);
+    background-color: #333;
     margin: 1.5rem 0;
 `
